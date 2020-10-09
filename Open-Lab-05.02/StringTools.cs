02c8b55cb364b3a5f2e3a27a36aa783a01f6849f
MilosM33 <milosmarejka89@gmail.com>
2020-10-09 20:31:50 +0200

    init

--- Open-Lab-05.02/StringTools.cs
+++ Open-Lab-05.02/StringTools.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-
+using System.Linq;
 namespace Open_Lab_05._02
 {
     public class StringTools
     {
         public string NoYelling(string sentence)
         {
-            throw new NotImplementedException();
+            string temp = "";
+            var array = sentence.Split(' ');
+            for (int y = 0; y < array.Length; y++)
+            {
+                if (y != array.Length-1)
+                {
+                    temp += array[y] + " ";
+                }
+                else
+                {
+                    string current = array[y];
+                    int indexQuestion = current.IndexOf('?');
+                    int indexExclamation = current.IndexOf('!');
+
+                    if (indexQuestion != -1)
+                    {
+                        temp += current.Substring(0, indexQuestion + 1);
+                    }
+                    else if (indexExclamation != -1)
+                    {
+                        temp += current.Substring(0, indexExclamation + 1);
+                    }
+                    else
+                        temp += current;
+                }
+            }
+            return temp;
         }
+        
     }
 }
